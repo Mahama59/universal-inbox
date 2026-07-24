44973966208691beb77bcc882465972c342e018b
Mahama59 <113291429+Mahama59@users.noreply.github.com>
2026-07-24 15:15:17 +0000

    Implement endpoint to retrieve all messages

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -1,1 +1,36 @@
+const express = require("express");
 
+const router = express.Router();
+
+
+const gmail =
+require("../services/gmail");
+
+
+const slack =
+require("../services/slack");
+
+
+
+// Get all messages
+
+router.get("/", (req,res)=>{
+
+
+    const messages = [
+
+        ...gmail.getGmailMessages(),
+
+        ...slack.getSlackMessages()
+
+    ];
+
+
+    res.json(messages);
+
+
+});
+
+
+
+module.exports = router;
