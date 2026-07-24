e341dbadac5353160bc92a01a64c5dbc8677773a
Mahama59 <113291429+Mahama59@users.noreply.github.com>
2026-07-24 15:18:34 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,23 @@
-const authRoutes = require("./routes/auth");
-
 const express = require("express");
 const cors = require("cors");
+
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
 
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
+
+
+// Routes
+
 app.use("/auth", authRoutes);
 
-const messages = [
+app.use("/messages", messageRoutes);
 
-{
-    id:1,
-    platform:"📧 Gmail",
-    sender:"John",
-    message:"Meeting reminder tomorrow",
-    status:"Unread",
-    starred:false
-},
-
-
-{
-    id:2,
-    platform:"💬 Slack",
-    sender:"Marketing Team",
-    message:"New campaign update",
-    status:"Unread",
-    starred:false
-}
-
-];
-
-
-app.get("/messages",(req,res)=>{
-
-    res.json(messages);
-
-});
 
 
 app.listen(3000,()=>{
